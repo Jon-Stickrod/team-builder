6e3e8f36f3f0e521b69a06fbcad445495fcb3e12
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-04-26 19:19:46 -0700

    list displaying

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './form';
+import Members from './Member';
 
 function App() {
+  const [members,setMembers] = useState([])
+
+  const addMember = member => {
+    setMembers([...members,member])
+    console.log(members)
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form addMember={addMember}/>
+      <Members members={members}/> 
     </div>
   );
 }
---
+++ team-builder/src/Member.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Members = props => {
+    return (
+            props.members.map((member,i) => {
+               return <div key={i}>{member}</div>
+            }))};
+
+export default Members;
